9de6a372c468bfcd4e702edea29429ce8c2c0441
Shawn Rice <rice.shawn@gmail.com>
2014-06-04 20:52:38 -0400

    changed settings input resizing js

--- gui/packal.php
+++ gui/packal.php
@@ -400,8 +400,37 @@ function settings() {
     <span id='workflow-reporting'></span>
     <span id='force-packal'></span>
     <span id='notification-options'></span>
+    <span id='username'></span>
+    <span id='authorName'></span>
 
     <script type='text/javascript' >
+      // From http://digitalzoomstudio.net/2013/06/19/calculate-text-width-with-jquery/
+      jQuery.fn.textWidth = function(){
+        var _t = jQuery(this);
+        var html_org = _t.html();
+        if(_t[0].nodeName=='INPUT'){
+            html_org = _t.val();
+        }
+        var html_calcS = '<span>' + html_org + '</span>';
+        jQuery('body').append(html_calcS);
+        var _lastspan = jQuery('span').last();
+        //console.log(_lastspan, html_calc);
+        _lastspan.css({
+            'font-size' : _t.css('font-size')
+            ,'font-family' : _t.css('font-family')
+        })
+        var width =_lastspan.width() + 5;
+        //_t.html(html_org);
+        _lastspan.remove();
+        return width;
+      };
+
+      $( 'input[type="text"]' ).each( function() {
+        id = $( this ).attr( 'name' );
+        $( '#' + id ).html( $( this ).val() );
+        width = $( '#' + id ).textWidth();
+        $( this ).css( 'width', width + 5 );
+      });
       $( document ).ready( function() {
         var selects = [ 
           'packal-username',
@@ -419,6 +448,7 @@ function settings() {
           $( '.packal-account' ).hide();
         }
       });
+
       $( '.packal-username' ).on( 'change', function() {
         value = $( this ).val();
         if ( value == 'do' ) {
@@ -446,7 +476,13 @@ function settings() {
       function resizeInput() {
         $( this ).attr( 'size', $( this ).val().length);
       }
-      $( 'input[type="text"]' ).keyup( resizeInput ).each( resizeInput );
+ 
+      $( 'input[type="text"]' ).keyup( function() {
+        id = $( this ).attr( 'name' );
+        $( '#' + id ).html( $( this ).val() );
+        width = $( '#' + id ).textWidth();
+        $( this ).css( 'width', width + 5 );
+      });
     </script>
   </div>
 
