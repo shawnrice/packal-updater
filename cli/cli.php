06a28c6af3cae33d1834821339ef86db6c8301b9
Shawn Patrick Rice <rice@shawnrice.org>
2016-11-22 09:18:05 -0500

    saving before work

--- cli/cli.php
+++ cli/cli.php
@@ -16,6 +16,7 @@ if ( ! ini_get( 'date.timezone' ) ) {
 }
 
 require_once( __DIR__ . '/../config.php' );
+require_once( __DIR__ . '/../Library/Alphred.phar' );
 require_once( __DIR__ . '/../autoloader.php' );
 
 /**
